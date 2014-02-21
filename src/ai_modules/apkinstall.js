a6142f6c2c676101e1c84f40ffd77a3258f9e08c
zhongwei.bzw <zhongwei.bzw@taobao.com>
2014-02-21 10:44:30 +0800

    async.series 改造

--- src/ai_modules/apkinstall.js
+++ src/ai_modules/apkinstall.js
@@ -17,31 +17,17 @@ apkinstall = {
                 localpath = $('#apkinstall-localfile')[0].files[0].path;
 
                 for (x in dev_list) {
-                    function fe(dev_id) {
-                        var devid = dev_id;
-                        ApkInfo.getPackageName(localpath, function (package_name) {
-                            console.info("installing " + package_name);
-                            if ($('#apkinstall-uninstallold').prop("checked")) {
-                                self.uninstall(devid, package_name, function (dev_id) {
-                                    self.install(devid, localpath)
-                                });
-                            } else {
-                                self.install(devid, localpath)
-                            }
-                        });
-                    }
-
-                    fe(dev_list[x]);
+                    self.batchC(dev_list[x], localpath, null);
                 }
             } else if ($('#apkinstall-url').val() != "") {
-                url_link = $('#apkinstall-url').val();
+                var url_link = $('#apkinstall-url').val();
                 var http = require('http');
                 var fs = require('fs');
 
-                var date = (new Date()).Format("yyyy-MM-dd_hh:mm:ss");
+                var date = (new Date()).Format("yyyy-MM-dd_hh-mm-ss");
                 var str = date;
 
-                localpath = process.cwd() + "/cache/" + "ai_" + str + ".apk";
+                var localpath = process.cwd() + "/cache/" + "ai_" + str + ".apk";
                 AI.showProgress("Downloading Files");
 
                 var file = fs.createWriteStream(localpath);
@@ -53,21 +39,7 @@ apkinstall = {
                         AI.hideProgress();
 
                         for (x in dev_list) {
-                            function fe(dev_id) {
-                                var devid = dev_id;
-                                ApkInfo.getPackageName(localpath, function (package_name) {
-                                    console.info("installing " + package_name);
-                                    if ($('#apkinstall-uninstallold').prop("checked")) {
-                                        self.uninstall(devid, package_name, function (dev_id) {
-                                            self.install(devid, localpath)
-                                        });
-                                    } else {
-                                        self.install(devid, localpath)
-                                    }
-                                });
-                            }
-
-                            fe(dev_list[x]);
+                            self.batchC(dev_list[x], localpath, null);
                         }
                     });
                 }).on('error', function (e) {
@@ -86,14 +58,36 @@ apkinstall = {
      * @param callback dev_id
      */
     uninstall: function (dev_id, packagename, callback) {
-        AI.deviceSimpleCommand(dev_id, " uninstall " + packagename, function (hasError, stdout, stderror) {
+        AI.deviceSimpleCommand(dev_id, "uninstall " + packagename, function (hasError, stdout, stderror) {
             console.log("uninstall " + packagename + " done " + stdout);
             if (callback != null) {
                 callback(dev_id);
             }
         });
     },
-
+    runPackage: function (dev_id, localpath, pcallback) {
+        var package_name = ""
+        var intent_name = ""
+        var async = require("async");
+        async.series([function (callback) {
+                ApkInfo.getPackageName(localpath, function (pacname) {
+                    package_name = pacname;
+                    callback();
+                });
+            }, function (callback) {
+                ApkInfo.getPackageStartIntent(localpath, function (intent) {
+                    intent_name = intent;
+                    callback();
+                });
+            }, function (callback) {
+                console.info("starting " + package_name + "/" + intent);
+                AI.deviceSimpleCommand(dev_id, "shell am start " + package_name + "/" + intent, function (hasError, stdout, stderror) {
+                    if (callback != null) {
+                        callback(dev_id);
+                    }
+                });
+            }],pcallback);
+    },
     /**
      * 安装某个包
      * @param dev_id
@@ -111,5 +105,37 @@ apkinstall = {
                 callback(dev_id);
             }
         });
+    },
+    batchC: function (dev_id, localpath, pcallback) {
+        console.info("installing " + localpath);
+        var async = require("async");
+        var self = this;
+        async.series([
+            // 是否需要卸载旧版本
+            function (callback) {
+                if ($('#apkinstall-uninstallold').prop("checked")) {
+                    ApkInfo.getPackageName(localpath, function (package_name) {
+                        self.uninstall(dev_id, package_name, function () {
+                            callback();
+                        });
+                    });
+                } else {
+                    callback();
+                }
+            },
+            // 进行安装
+            function (callback) {
+                self.install(dev_id, localpath, function (devid) {
+                    callback();
+                });
+            },
+            // 是否启动
+            function (callback) {
+                if ($('#apkinstall-runafter').prop("checked")) {
+                    self.runPackage(dev_id, localpath);
+                }
+                callback();
+            }],
+            pcallback);
     }
 }
